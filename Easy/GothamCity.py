2e9bf1222405ce7f8fe591da1b9e898a7a4b84c9
pyskye <80241738+pyskye@users.noreply.github.com>
2021-06-27 13:30:01 +0900

    Add files via upload

---
+++ Easy/GothamCity.py
@@ -0,0 +1,8 @@
+report = int(input())
+
+if report < 5:
+    print("I got this!")
+elif report >= 5 and report <= 10:
+    print("Help me Batman")
+elif report > 10:
+    print("Good Luck out there!")
